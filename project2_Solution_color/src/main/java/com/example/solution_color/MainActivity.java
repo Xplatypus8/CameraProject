9ad0a0f01e2d26201f53d419f5e38ca6c012d0da
xplatypus8 <clifton.dent.15@cnu.edu>
2017-02-24 20:42:43 -0500

    Attempted to make action bar transparent

--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity  {
     @Override
@@ -14,12 +14,13 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setAlpha((float).5);
         setSupportActionBar(myToolbar);
 
     }
 
-    private void setSupportActionBar(Toolbar myToolbar) {
-    }
+    //private void setSupportActionBar(Toolbar myToolbar) {
+    //}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
