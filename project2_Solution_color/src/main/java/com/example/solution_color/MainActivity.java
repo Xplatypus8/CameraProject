f77e5f2cadc1881b129d6b40026f766963fcc764
KyleHoobler <Kyle.hoobler.15@cnu.edu>
2017-02-28 17:24:29 -0500

    Got Custom Text Working

--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.solution_color;
 
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -20,44 +17,63 @@ import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-
 import com.library.bitmap_utilities.BitMap_Helpers;
-
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+/**
+ * Android Project 2 By Kyle Hoobler and Taylor Dent
+ *
+ */
 public class MainActivity extends AppCompatActivity{
 
+    //Custom share phrase
     private String phrase;
+    private String subject;
+    //saturation levels
     private int saturation;
+    //Black and white settings
     private int percentage;
+    //out file
     private File file;
     private String imagePath = "";
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView background;
 
 
+    /**
+     * Starts the application and performs default tasks
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
+        //locks screen
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //Loads XML
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Tool bar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setAlpha((float) .7);
         setSupportActionBar(myToolbar);
+        //Sets background to default image/picture
         background = (ImageView)findViewById(R.id.imageView);
 
     }
 
 
-
+    /**
+     * Creates menu
+     *
+     * @param menu options
+     * @return if menu launched
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -65,6 +81,11 @@ public class MainActivity extends AppCompatActivity{
         return true;
     }
 
+    /**
+     * Checks if options selected from list
+     * @param item item in toolbar
+     * @return return whether valid operation
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -78,17 +99,27 @@ public class MainActivity extends AppCompatActivity{
         return true;
     }
 
+    /**
+     * Resets the background to default state
+     * @param item
+     */
     public void reset(MenuItem item){
 
-
+        //supposed to delete image
         Camera_Helpers.delSavedImage(imagePath);
+        //sets to default background
         background.setImageResource(R.drawable. gutters );
+        //Scales back to normal in case of error
         background.setScaleType(ImageView.ScaleType.FIT_CENTER);
         background.setScaleType(ImageView.ScaleType.FIT_XY);
 
     }
 
-
+    /**
+     * Launches picture intent
+     *
+     * @param view
+     */
     public void dispatchTakePictureIntent(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -97,22 +128,37 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
+    /**
+     * Changes the picture to blacka and white, calls helper method bitmap helpers
+     * @param item
+     */
     public void blackAndWhite(MenuItem item){
+        //Gets value from prefernces
         percentage = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(this).getString("sketchLevel", "50"));
+        //Checks if valid percentage
         if(percentage > 100){
             percentage = 100;
         }
         else if(percentage < 0 ){
             percentage = 0;
         }
+        //Copies the background
         Bitmap bmp = BitMap_Helpers.copyBitmap(background.getDrawable());
+        //Changes to B/W
         bmp = BitMap_Helpers.thresholdBmp(bmp, percentage);
+        //Sets background
         Drawable d = new BitmapDrawable(getResources(), bmp);
         background.setImageDrawable(d);
     }
 
+    /**
+     * Adds saturation to the image on screen
+     * @param item
+     */
     public void colorize(MenuItem item){
+        //Saturation levels
         saturation = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(this).getString("saturation","50"));
+        //Checks if valid saturation
         if(saturation > 255){
             saturation = 255;
         }
@@ -120,16 +166,26 @@ public class MainActivity extends AppCompatActivity{
             saturation = 0;
         }
 
+        //Copies background
         Bitmap tmp = BitMap_Helpers.copyBitmap(background.getDrawable());
+        //adds the threshold
         Bitmap thresh = BitMap_Helpers.thresholdBmp(tmp, saturation);
         Bitmap color = BitMap_Helpers.colorBmp(tmp, saturation);
         BitMap_Helpers.merge(color,thresh);
+
+        //Sets it to background
         Drawable back = new BitmapDrawable((getResources()), color);
         background.setImageDrawable(back);
 
     }
 
-
+    /**
+     * Result of Camera intent launch
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
@@ -140,6 +196,7 @@ public class MainActivity extends AppCompatActivity{
             catch (IOException e){
 
             }
+            //Sets background to new image
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 background.setImageBitmap(imageBitmap);
@@ -148,21 +205,29 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-
-
-
+    /**
+     * Share menu
+     * TODO: Get Image sharing working
+     * @param item
+     */
     public void share(MenuItem item){
 
+        //Create file path and get the picture(not working)
         file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), imagePath);
+        //Get the Message from prefrences
         phrase = PreferenceManager.getDefaultSharedPreferences(this).getString("shareMessage","I made this.");
+        subject = PreferenceManager.getDefaultSharedPreferences(this).getString("shareTitle", "This is the subject of the message you would like to share");
+        //Make new intent
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.setType("image/jpg");
+
+        //Get file path
         Uri uri = Uri.fromFile(file);
         shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
 
-
-        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My Picture");
+        //Share text/Subject line
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
         shareIntent.putExtra(Intent.EXTRA_TEXT, phrase);
         startActivity(shareIntent);
 
